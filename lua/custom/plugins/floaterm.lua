09059ea536c1cabb8f0be9f992827a9840dce1a6
Marco Tormento <mtormento80@gmail.com>
2023-11-07 10:11:52 +0100

    Set window defaults for floaterm and changed key binding

--- lua/custom/plugins/floaterm.lua
+++ lua/custom/plugins/floaterm.lua
@@ -1,10 +1,13 @@
+vim.g.floaterm_width = 0.9
+vim.g.floaterm_height = 0.9
+
 return {
 	"voldikss/vim-floaterm",
 	version = "*",
 	config = function()
-		vim.keymap.set('t', '<C-[>', '<C-\\><C-n>', { silent = true })
-		vim.keymap.set('n', '<F10>', ':FloatermNew --width=0.9 --height=0.9<cr>', { silent = true })
-		vim.keymap.set('t', '<F10>', '<C-\\><C-n>:FloatermNew --width=0.9 --height=0.9<cr>', { silent = true })
+		vim.keymap.set('t', '<C-]>', '<C-\\><C-n>', { silent = true })
+		vim.keymap.set('n', '<F10>', ':FloatermNew<cr>', { silent = true })
+		vim.keymap.set('t', '<F10>', '<C-\\><C-n>:FloatermNew<cr>', { silent = true })
 		vim.keymap.set('n', '<F11>', ':FloatermNext<cr>', { silent = true })
 		vim.keymap.set('t', '<F11>', '<C-\\><C-n>:FloatermNext<cr>', { silent = true })
 		vim.keymap.set('n', '<F12>', ':FloatermToggle<cr>', { silent = true })
